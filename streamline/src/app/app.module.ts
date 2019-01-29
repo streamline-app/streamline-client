3b97e1f3848ce0b7b774dbc9918314b7e256243e
Dan Morton <dmorton2297@gmail.com>
2019-01-28 20:50:26 -0500

    Created homecontroller and included material in styles.css

--- streamline/src/app/app.module.ts
+++ streamline/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { MaterialModule } from './material/material.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-  { path: 'home', component: AppComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
