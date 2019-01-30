7c3e758614878b77d42fc187b048d583a09c20c0
Scott Merritt <smerrit@purdue.edu>
2019-01-30 18:52:56 -0500

    infra -- added route to profile page and improved side-bar layout

--- streamline/src/app/app.module.ts
+++ streamline/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TagsComponent } from './tags/tags.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'tags', component: TagsComponent }
+  { path: 'tags', component: TagsComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
@@ -16,6 +18,7 @@ const appRoutes: Routes = [
     AppComponent,
     HomeComponent,
     TagsComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
