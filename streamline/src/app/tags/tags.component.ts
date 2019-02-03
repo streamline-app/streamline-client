bffdb99dcaf9a149ae84f8a144f8980fa469c56a
Scott Merritt <smerrit@purdue.edu>
2019-02-03 17:49:31 -0500

    infra -- basic front end of tags page, need to connect to backend now

--- streamline/src/app/tags/tags.component.ts
+++ streamline/src/app/tags/tags.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 
+/* Stub filler for Tags */
+const TAGS: Tag[] = [
+  { id: 11, name: 'CS homework', description: 'something something' },
+  { id: 12, name: 'Laundry', description: 'another desc' },
+  { id: 13, name: 'Gym', description: 'filler text' },
+];
+
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
   styleUrls: ['./tags.component.css']
 })
 export class TagsComponent implements OnInit {
+  opened: boolean;
+  tags: Tag[];
+  selectedTag: Tag;
 
-  constructor() { }
+  constructor() {
+    this.tags = TAGS;
+    this.opened = false;
+   }
 
   ngOnInit() {
   }
 
+  createTag(){
+    alert("hello");
+  }
+
+  onSelect(tag: Tag): void {
+    if(this.opened){ //if already open, close it.
+      this.opened = false;
+    }
+
+    //load data for that tag ------
+    this.selectedTag = tag;
+
+    //open tab again
+    this.opened = true;
+  }
 }
+
+
+interface Tag {
+  id: number,
+  name: string,
+  description: string
+  /* add later when analytics are applicable
+    tasks_comp: int,
+    average_time: double,
+    average_acc: double,
+    task_overunder: double,
+    color?
+  */
+};
