26bc1a428e70758967e045a3eceac35682cbb7f0
Dan Morton <dmorton2297@gmail.com>
2019-02-06 20:13:21 -0500

    all tests passing

--- streamline/src/app/backend.service.spec.ts
+++ streamline/src/app/backend.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { BackendService } from './backend.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material/material.module';
+
 
 describe('BackendService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BackendService]
+      providers: [BackendService],
+      imports: [HttpClientModule, MaterialModule],
     });
   });
 
--- streamline/src/app/login/login.component.spec.ts
+++ streamline/src/app/login/login.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MaterialModule } from '../material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +16,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      imports: [ ReactiveFormsModule, HttpClientModule, FormsModule, MaterialModule, BrowserAnimationsModule]
     })
     .compileComponents();
   }));
