e994e50db37af42fa978597b4335fec86c66a945
Scott Merritt <smerrit@purdue.edu>
2019-02-13 23:30:00 -0500

    feat -- added autofill in edit dialog; added color option to create dialog; cleaned up some unnecessary extra code

--- streamline/src/app/tags/tags.component.ts
+++ streamline/src/app/tags/tags.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { BackendService } from '../backend.service';
-import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
 
 
 interface Tag {
@@ -15,15 +15,10 @@ interface Tag {
   userID: number //NOTE: this value is hardcoded until userID can be pulled from auth
 };
 
-interface TagEdit {
-  name:string,
-  desc: string,
-  color: string
-};
-
 export interface CreateTagDialogData {
   name: string,
-  desc: string
+  desc: string,
+  color: string
 };
 
 export interface EditTagDialogData {
@@ -41,33 +36,15 @@ export class TagsComponent implements OnInit {
   opened: boolean;
   tags: Tag[];
   selectedTag: Tag;
-  newTag: Tag;
-  editedtTag: TagEdit;
-
 
   constructor(private backend: BackendService,
-    public create_dialog: MatDialog) {
+    public create_dialog: MatDialog,
+    private snackbar: MatSnackBar
+  ){
+    //make sure sidenav is closed
     this.opened = false;
 
-    this.newTag = {
-      //set init values for new tag
-      id: 1,
-      name: "",
-      description: "",
-      tasks_comp: 0,
-      average_time: 0,
-      average_acc: 0,
-      task_overunder: 1.2,
-      color: '#c4c4c4',
-      userID: 1
-    }
-
-    this.editedtTag = {
-      name: "",
-      desc: "",
-      color: ""
-    }
-
+    //fill display
     this.getUserTags();
   }
 
@@ -77,25 +54,41 @@ export class TagsComponent implements OnInit {
   createTag() {
     const dialogRef = this.create_dialog.open(CreateTagDialog, {
       width: '250px',
-      data: { name: '', desc: '' }
+      data: { name: '', desc: '', color: "#c4c4c4" }
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('closed create tag dialog');
-      //get result info here
-      console.log('recieved: ' + result.name + ' ' + result.desc);
       if (result != null) {
-        this.newTag.name = result.name;
-        this.newTag.description = result.desc;
+        //construct Tag object with input values
+        let newTag: Tag = {
+          id: 1,
+          name: result.name,
+          description: result.desc,
+          tasks_comp: 0,
+          average_time: 0,
+          average_acc: 0,
+          task_overunder: 0,
+          color: result.color,
+          userID: 1
+        }
 
-        console.log(this.newTag);
+        console.log(newTag);
         //send data to backend
-        this.backend.createTag(this.newTag).subscribe(tag => window.alert('Tag Created'));
+        this.backend.createTag(newTag).subscribe(result => {
+
+          //three second snackbar pop up notification
+          let snackbarRef = this.snackbar.open('Tag Created!', 'Ok', { duration: 3000 });
+
+        }, error => {
+          console.log(error.message);
+          //three second snackbar pop up notification
+          let snackbarRef = this.snackbar.open('Oh no, something went wrong!', 'Ok', { duration: 3000 });
+        });
 
         //update display with new tag
         this.getUserTags();
       }
-      else { /* Do Nothing */}
+      else { /* Do Nothing */ }
     });
 
 
@@ -112,64 +105,74 @@ export class TagsComponent implements OnInit {
     this.opened = true;
   }
 
-  getUserTags(){
+  getUserTags() {
     this.backend.getUserTags("1").subscribe(result => { //TODO change userID
       console.log(result);
       //window.alert('Got Tags');
 
       //set display to show result
       this.tags = result;
-   
+
+    }, error => {
+      console.log(error.message);
+      //three second snackbar pop up notification
+      let snackbarRef = this.snackbar.open('Oh no, something went wrong!', 'Ok', { duration: 3000 });
     });
   }
 
-  deleteTag(id: number){
+  deleteTag(id: number) {
     const dialogRef = this.create_dialog.open(DeleteConfirmDialog, {
       width: '325px',
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      if(result){ //if confirmed, delete
-        this.backend.deleteTag(id).subscribe(result =>{
+      if (result) { //if confirmed, delete
+        this.backend.deleteTag(id).subscribe(result => {
           console.log(result);
-          window.alert("Tag Deleted!");
-    
+
+          //three second snackbar pop up notification
+          let snackbarRef = this.snackbar.open('Tag Deleted!', 'Ok', { duration: 3000 });
+
           //update display
           this.getUserTags();
 
           //close sidebar showing deleted tags details
           this.opened = false;
+        }, error => {
+          console.log(error.message);
+          //three second snackbar pop up notification
+          let snackbarRef = this.snackbar.open('Oh no, something went wrong!', 'Ok', { duration: 3000 });
         });
       }
-      else{ /*do nothing */ }
+      else { /*do nothing */ }
     });
   }
 
-  editTag(id: number){
+  editTag(id: number) {
     const dialogRef = this.create_dialog.open(EditTagDialog, {
       width: '325px',
+      data: { name: this.selectedTag.name, desc: this.selectedTag.description, color: this.selectedTag.color } /* Make sure to set fields to details of selected tag*/
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      if(result != null){
-        //construct TagEdit object with result values from dialog
-        this.editedtTag = { 
-          name: result.name,
-          desc: result.desc,
-          color: result.color
-        }
+      if (result != null) {
+        //close sidebar showing old tags details
+        this.opened = false;
 
-        //send to backend
-        this.backend.editTag(id, this.editedtTag).subscribe(res => {
+        //result is formatted to send directly to backend
+        this.backend.editTag(id, result).subscribe(res => {
           console.log('tag ' + id + ' udpated');
 
-          window.alert('Tag Updated!');
+          //three second snackbar pop up notification
+          let snackbarRef = this.snackbar.open('Tag Updated!', 'Ok', { duration: 3000 });
 
           //update display
           this.getUserTags();
+        }, error => {
+          console.log(error.message);
+          //three second snackbar pop up notification
+          let snackbarRef = this.snackbar.open('Oh no, something went wrong!', 'Ok', { duration: 3000 });
 
-          //close sidebar showing old tags details
-          this.opened = false;
         })
 
       }
@@ -209,11 +212,11 @@ export class DeleteConfirmDialog {
 
   constructor(public dialogRef: MatDialogRef<DeleteConfirmDialog>) { }
 
-  yes(){
+  yes() {
     this.dialogRef.close(true); //return true to parent component
   }
 
-  no(){
+  no() {
     this.dialogRef.close(false); //return false to parent component
   }
 }
@@ -225,19 +228,13 @@ export class DeleteConfirmDialog {
 })
 export class EditTagDialog {
   constructor(public dialogRef: MatDialogRef<EditTagDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: EditTagDialogData) {
-      this.data = {
-        name: "",
-        desc: "",
-        color: "#000000"
-      }
-     }
+    @Inject(MAT_DIALOG_DATA) public data: EditTagDialogData) { }
 
-    editTag(){
-      this.dialogRef.close(this.data); //return data fields to parent
-    }
+  editTag() {
+    this.dialogRef.close(this.data); //return data fields to parent
+  }
 
-    closeDialog(){
-      this.dialogRef.close();
-    }
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
