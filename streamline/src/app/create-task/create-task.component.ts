22039c56dd9c8e4cae7fb54be7e73fa5061fe3f7
Scott Merritt <smerrit@purdue.edu>
2019-02-18 23:41:23 -0500

    quickfix -- added snackbar for success/failure of create task rather than window alert

--- streamline/src/app/create-task/create-task.component.ts
+++ streamline/src/app/create-task/create-task.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { BackendService } from '../backend.service';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 
 @Component({
@@ -12,17 +13,17 @@ import { Router } from '@angular/router';
 })
 export class CreateTaskComponent {
 
-  public task : FormGroup = new FormGroup({
+  public task: FormGroup = new FormGroup({
     title: new FormControl(''),
     body: new FormControl(''),
     estimatedMin: new FormControl(0),
     estimatedHour: new FormControl(0)
   });
 
-  constructor(private backend: BackendService, private auth: AuthService, private router: Router) { }
+  constructor(private backend: BackendService, private auth: AuthService, private router: Router, private snackbar: MatSnackBar) { }
 
-  public onSubmit() {    
-    let task : any = {
+  public onSubmit() {
+    let task: any = {
       title: this.task.controls['title'].value,
       body: this.task.controls['body'].value,
       estimatedMin: this.task.controls['estimatedMin'].value,
@@ -30,15 +31,20 @@ export class CreateTaskComponent {
       userID: this.auth.getUserId()
     }
 
-    this.backend.createTask(task).subscribe(task => { 
-      window.alert('Task Created');
+    this.backend.createTask(task).subscribe(task => {
+      //three second snackbar pop up notification
+      let snackbarRef = this.snackbar.open('Task Created!', 'Ok', { duration: 3000 });
 
       this.router.navigateByUrl('/home');
+    }, error => {
+      console.log(error.message);
+      //three second snackbar pop up notification
+      let snackbarRef = this.snackbar.open('Oh no, something went wrong!', 'Ok', { duration: 3000 });
     });
 
   }
 
-  public onCancel(){
+  public onCancel() {
     this.router.navigateByUrl('/home');
   }
 }
