e028dea65367db13f24018df4b5c064bf1b4d48f
Dan Morton <dmorton2297@gmail.com>
2019-02-22 00:19:09 -0500

    Testing

--- streamline/src/app/sign-up/sign-up.component.spec.ts
+++ streamline/src/app/sign-up/sign-up.component.spec.ts
@@ -7,16 +7,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { APP_BASE_HREF } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+
+
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
   let fixture: ComponentFixture<SignUpComponent>;
 
+  const appRoutes: Routes = [
+    { path: 'home', component: HomeComponent },
+  ];
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignUpComponent ],
+      declarations: [ SignUpComponent, HomeComponent ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-      imports: [ ReactiveFormsModule, HttpClientModule, FormsModule, MaterialModule, BrowserAnimationsModule]
+      providers: [{provide: APP_BASE_HREF, useValue: '/'}],
+      imports: [ ReactiveFormsModule, HttpClientModule, FormsModule, MaterialModule, BrowserAnimationsModule,
+      RouterModule.forRoot(
+        appRoutes,
+        {enableTracing: true}
+      )
+      ]
     })
     .compileComponents();
   }));
