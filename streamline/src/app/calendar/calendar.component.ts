25e521d916d10a22cad74c2e21b57a3ec4bbf8d5
Scott Merritt <smerrit@purdue.edu>
2019-04-22 21:39:34 -0400

    fix -- removed example code from calendar

--- streamline/src/app/calendar/calendar.component.ts
+++ streamline/src/app/calendar/calendar.component.ts
@@ -91,48 +91,7 @@ export class CalendarComponent {
 
   refresh: Subject<any> = new Subject();
   events: CalendarEvent[] = [];
-  /* Example Events ---- TODO remove before merge
-    events: CalendarEvent[] = [
-      {
-        start: subDays(startOfDay(new Date()), 1),
-        end: addDays(new Date(), 1),
-        title: 'A 3 day event',
-        color: colors.red,
-        actions: this.actions,
-        allDay: true,
-        resizable: {
-          beforeStart: true,
-          afterEnd: true
-        },
-        draggable: true
-      },
-      {
-        start: startOfDay(new Date()),
-        title: 'An event with no end date',
-        color: colors.yellow,
-        actions: this.actions
-      },
-      {
-        start: subDays(endOfMonth(new Date()), 3),
-        end: addDays(endOfMonth(new Date()), 3),
-        title: 'A long event that spans 2 months',
-        color: colors.blue,
-        allDay: true
-      },
-      {
-        start: addHours(startOfDay(new Date()), 2),
-        end: new Date(),
-        title: 'A draggable and resizable event',
-        color: colors.yellow,
-        actions: this.actions,
-        resizable: {
-          beforeStart: true,
-          afterEnd: true
-        },
-        draggable: true
-      }
-    ];
-  */
+
   activeDayIsOpen: boolean = true;
 
   private tasks: Task[] = [];
